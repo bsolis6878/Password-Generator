38c4aa50d870a8ffbc72c42a7e01a8f2f93dd690
bsolis6878 <bsolis6878@gmail.com>
2022-07-31 20:30:27 -0500

    Added user prompts for what they would like in password

--- Develop/script.js
+++ Develop/script.js
@@ -4,18 +4,30 @@ function generatePassword() {
   // repeats prompt until valid input is entered
   while (true) {
   // ask user how long they would like password to be
-  var length = window.prompt("How long would you like your password to be? Please enter a numerical value between 8 and 128.")
+    var length = window.prompt("How long would you like your password to be? Please enter a numerical value between 8 and 128.")
   // turns input into number
-  var passwordLength = Number(length)
-  console.log(passwordLength)
-
-  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
-    window.alert("Please input a valid numerical value between 8 and 128.")
-  }
-  else {
-    break
+    var passwordLength = Number(length)
+    console.log(passwordLength)
+  
+  // repeats prompt if input isn't between 8 and 128
+    if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
+      window.alert("Please input a valid numerical value between 8 and 128.")
+    }
+    else {
+      break
   }
 }
+
+// variables for what user would like in their password
+var uppercase = window.confirm("Would you like to include uppercase letters in your password? Click 'OK' for yes or 'Cancel' for no.")
+console.log(uppercase)
+var lowercase = window.confirm("Would you like to include lowercase letters in your password? Click 'OK' for yes or 'Cancel' for no.")
+console.log(lowercase)
+var numbers = window.confirm("Would you like to include numbers in your password? Click 'OK' for yes or 'Cancel' for no.")
+console.log(numbers)
+var specials = window.confirm("Would you like to include specials in your password? Click 'OK' for yes or 'Cancel' for no.")
+console.log(specials)
+
 }
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
